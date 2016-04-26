688a8b653c591c3fd6ab8d2144bd7b5a17fd53a0
Rafael Ramos <rramo012@gmail.com>
2016-04-26 14:18:51 -0400

    Formatting Line Endings

--- boldgrid/includes/class-boldgrid-ninja-forms.php
+++ boldgrid/includes/class-boldgrid-ninja-forms.php
@@ -23,19 +23,19 @@ class Boldgrid_Ninja_Forms {
 	 * @var Boldgrid_Ninja_Forms_Config
 	 */
 	private $boldgrid_ninja_form_config;
-	
+
 	/**
 	 * A full array of tab configurations
 	 *
 	 * @var array
 	 */
 	protected $tab_configs;
-	
+
 	/**
 	 * Path configurations used for the plugin
 	 */
 	protected $path_configs;
-	
+
 	/**
 	 * Accessor for tab configs
 	 *
@@ -44,7 +44,7 @@ class Boldgrid_Ninja_Forms {
 	public function get_tab_configs() {
 		return $this->tab_configs;
 	}
-	
+
 	/**
 	 * Accessor for path configs
 	 *
@@ -53,41 +53,41 @@ class Boldgrid_Ninja_Forms {
 	public function get_path_configs() {
 		return $this->path_configs;
 	}
-	
+
 	/**
 	 * Initialize tab configs
 	 */
 	public function __construct() {
 		$plugin_dir = self::derive_plugin_dir();
-		
+
 		$plugin_filename = $plugin_dir . '/ninja-forms.php';
-		
+
 		$this->tab_configs = include $plugin_dir . '/boldgrid/includes/config/layouts.php';
-		
+
 		$this->path_configs = array (
 			'plugin_dir' => $plugin_dir,
-			'plugin_filename' => $plugin_filename 
+			'plugin_filename' => $plugin_filename
 		);
-		
+
 		// Identify the BoldGrid Form config directory:
 		$settings = array (
-			'configDir' => $plugin_dir . '/boldgrid/includes/config' 
+			'configDir' => $plugin_dir . '/boldgrid/includes/config'
 		);
 		// Load and instantiate WPB_Form_Config:
 		require_once $plugin_dir . '/boldgrid/includes/class-boldgrid-ninja-forms-config.php';
-		
+
 		// Instantiate the Boldgrid_Ninja_Forms_Config class and save it into a class property:
 		$this->boldgrid_ninja_form_config = new Boldgrid_Ninja_Forms_Config( $settings );
-		
+
 		// Add an action to load this plugin on init, only in the dashboard:
 		if ( is_admin() ) {
 			add_action( 'init', array (
 				$this,
-				'load_boldgrid_ninja_form_update' 
+				'load_boldgrid_ninja_form_update'
 			) );
 		}
 	}
-	
+
 	/**
 	 * Load the BoldGrid Ninja Forms update class
 	 *
@@ -97,10 +97,10 @@ class Boldgrid_Ninja_Forms {
 		// Load and check for plugin updates:
 		require_once BOLDGRID_NINJA_FORMS_PATH .
 			 '/boldgrid/includes/class-boldgrid-ninja-forms-update.php';
-		
+
 		$plugin_update = new Boldgrid_Ninja_Forms_Update( $this );
 	}
-	
+
 	/**
 	 * Get $boldgrid_ninja_form_config class property
 	 *
@@ -109,7 +109,7 @@ class Boldgrid_Ninja_Forms {
 	public function get_boldgrid_ninja_form_config() {
 		return $this->boldgrid_ninja_form_config;
 	}
-	
+
 	/**
 	 * Get plugin directory
 	 *
@@ -120,7 +120,7 @@ class Boldgrid_Ninja_Forms {
 	public static function derive_plugin_dir() {
 		return realpath( dirname( dirname( dirname( __FILE__ ) ) ) );
 	}
-	
+
 	/**
 	 * Javascript needed for editor
 	 *
@@ -128,23 +128,23 @@ class Boldgrid_Ninja_Forms {
 	 */
 	public function enqueue_header_content() {
 		global $pagenow;
-		
+
 		if ( false == in_array( $pagenow, array (
 			'post.php',
-			'post-new.php' 
+			'post-new.php'
 		) ) ) {
 			return;
 		}
-		
-		wp_enqueue_script( 'media-imhwpb', 
-			plugins_url( '/boldgrid/assets/js/media.js', $this->path_configs['plugin_filename'] ), 
+
+		wp_enqueue_script( 'media-imhwpb',
+			plugins_url( '/boldgrid/assets/js/media.js', $this->path_configs['plugin_filename'] ),
 			array (), BOLDGRID_NINJA_FORM_VERSION, true );
-		
-		wp_enqueue_script( 'boldgrid-form-shortcode', 
-			plugins_url( '/boldgrid/assets/js/shortcode.js', 
+
+		wp_enqueue_script( 'boldgrid-form-shortcode',
+			plugins_url( '/boldgrid/assets/js/shortcode.js',
 				$this->path_configs['plugin_filename'] ), array (), BOLDGRID_NINJA_FORM_VERSION, true );
 	}
-	
+
 	/**
 	 * Initialization hook for BoldGrid Ninja forms
 	 *
@@ -152,32 +152,32 @@ class Boldgrid_Ninja_Forms {
 	 */
 	public function init() {
 		global $pagenow;
-		
+
 		add_action( 'admin_init', array (
 			$this,
-			'admin_init' 
+			'admin_init'
 		) );
-		
-		add_filter( 'ninja_forms_starter_form_contents', 
+
+		add_filter( 'ninja_forms_starter_form_contents',
 			array (
 				$this,
-				'modify_starter_forms' 
+				'modify_starter_forms'
 			) );
-		
+
 		if ( 'edit.php' == $pagenow ) {
 			add_action( 'pre_get_posts', array (
 				$this,
-				'remove_ninja_preview_page' 
+				'remove_ninja_preview_page'
 			) );
-			
-			add_filter( 'wp_count_posts', 
+
+			add_filter( 'wp_count_posts',
 				array (
 					$this,
-					'remove_preview_page_from_page_count' 
+					'remove_preview_page_from_page_count'
 				), 10, 2 );
 		}
 	}
-	
+
 	/**
 	 * Initialization process for administration section if Boldgrid Ninja Forms
 	 *
@@ -187,47 +187,47 @@ class Boldgrid_Ninja_Forms {
 		$valid_pages = array (
 			'post.php',
 			'post-new.php',
-			'media-upload.php' 
+			'media-upload.php'
 		);
-		
+
 		$edit_post_page = in_array( basename( $_SERVER['SCRIPT_NAME'] ), $valid_pages );
 		if ( is_admin() && $edit_post_page ) {
-			
+
 			// Create Media Modal Tabs
 			$this->create_tabs();
-			
+
 			// Print all forms as media templates
-			add_action( 'print_media_templates', 
+			add_action( 'print_media_templates',
 				array (
 					$this,
-					'print_media_templates' 
+					'print_media_templates'
 				) );
-			
+
 			// load up any css / js we need
-			add_action( 'admin_enqueue_scripts', 
+			add_action( 'admin_enqueue_scripts',
 				array (
 					$this,
-					'enqueue_header_content' 
+					'enqueue_header_content'
 				), 15 );
-			
+
 			// Add Css to hide and show title/description
 			$this->editor_styles();
 		}
 	}
-	
+
 	/**
 	 * Add Css to hide and show title/description
 	 *
 	 * @return void
 	 */
 	public function editor_styles() {
-		add_editor_style( 
+		add_editor_style(
 			plugins_url( '/boldgrid/assets/css/editor.css', $this->path_configs['plugin_filename'] ) );
-		
-		add_editor_style( 
+
+		add_editor_style(
 			plugins_url( '/css/ninja-forms-display.css', $this->path_configs['plugin_filename'] ) );
 	}
-	
+
 	/**
 	 * Static method that will add all forms that have been defined in the
 	 * boldgrid/includes/prebuilt-forms folder
@@ -239,101 +239,101 @@ class Boldgrid_Ninja_Forms {
 	public static function add_prebuilt_forms() {
 		// Locate the prebuilt-forms directory:
 		$prebuilt_forms_directory = self::derive_plugin_dir() . '/boldgrid/includes/prebuilt-forms';
-		
+
 		// If there is no directory found, then return:
 		if ( false === is_dir( $prebuilt_forms_directory ) ) {
 			return;
 		}
-		
+
 		// Get a directory listing:
 		$prebuilt_form_directory_listing = scandir( $prebuilt_forms_directory );
-		
+
 		// Natural sort order
 		natsort( $prebuilt_form_directory_listing );
-		
+
 		// If there are no listings, then return:
 		if ( empty( $prebuilt_form_directory_listing ) ) {
 			return;
 		}
-		
+
 		// Determinte the prebuilt form files:
-		$prebuilt_form_files = array_diff( $prebuilt_form_directory_listing, 
+		$prebuilt_form_files = array_diff( $prebuilt_form_directory_listing,
 			array (
 				'..',
 				'.',
-				'index.php' 
+				'index.php'
 			) );
-		
+
 		// If there are no listings after filtering, then return:
 		if ( empty( $prebuilt_form_files ) ) {
 			return;
 		}
-		
+
 		// Get the site's title:
 		$site_title = get_bloginfo( 'name' );
-		
+
 		// Get the site's email address:
 		$email_address = get_bloginfo( 'admin_email' );
-		
+
 		// Initialize $notifications_array:
 		$notifications_array = array ();
-		
+
 		// Iterate through each form file and import:
 		foreach ( $prebuilt_form_files as $prebuilt_form_file ) {
 			// Read the form file:
-			$prebuilt_form = file_get_contents( 
+			$prebuilt_form = file_get_contents(
 				$prebuilt_forms_directory . '/' . $prebuilt_form_file );
-			
+
 			// Unserialize the form data:
 			$prebuilt_form_unserialized = unserialize( $prebuilt_form );
-			
+
 			// If the form data is invalid, then skip:
 			if ( false === $prebuilt_form_unserialized ) {
 				continue;
 			}
-			
+
 			// Initialize $updated_notification:
 			$updated_notification = false;
-			
+
 			// Update the array:
 			if ( false === empty( $prebuilt_form_unserialized['notifications'] ) ) {
 				// Update existing email notifications:
 				foreach ( $prebuilt_form_unserialized['notifications'] as $n_index => $notification ) {
 					if ( 'email' == $notification['type'] ) {
-						$prebuilt_form_unserialized['notifications'][$n_index]['date_updated'] = date( 
+						$prebuilt_form_unserialized['notifications'][$n_index]['date_updated'] = date(
 							'Y-m-d' );
 						$prebuilt_form_unserialized['notifications'][$n_index]['from_name'] = $site_title;
 						$prebuilt_form_unserialized['notifications'][$n_index]['from_address'] = $email_address;
 						$prebuilt_form_unserialized['notifications'][$n_index]['to'] = $email_address;
-						
-						if ( empty( 
+
+						if ( empty(
 							$prebuilt_form_unserialized['notifications'][$n_index]['email_subject'] ) ) {
 							$prebuilt_form_unserialized['notifications'][$n_index]['email_subject'] = 'Form submission';
 						}
-						
+
 						$updated_notification = true;
 					}
 				}
-				
+
 				// Move notifications from the import data to $notifications_array.
 				$notification_array_temp = $prebuilt_form_unserialized['notifications'];
-				
+
 				unset( $prebuilt_form_unserialized['notifications'] );
 			}
-			
+
 			// Serialize the form data:
 			$prebuilt_form = serialize( $prebuilt_form_unserialized );
-			
+
 			// Import the form:
 			$last_form_id = ninja_forms_import_form( $prebuilt_form );
-			
+
 			// Move $notification_array_temp to $notifications_array[$last_form_id].
 			if ( false === empty( $notification_array_temp ) ) {
 				$notifications_array[$last_form_id] = $notification_array_temp;
-				
+
 				unset( $notifications_array_temp );
 			}
-			
+
 			// If an email notification does not exist, then add one to $notifications_array:
 			// Array index has no meaning; the import process does not use the key value.
 			if ( false === $updated_notification ) {
@@ -353,12 +353,12 @@ class Boldgrid_Ninja_Forms {
 					'cc' => '',
 					'bcc' => '',
 					'redirect_url' => '',
-					'success_msg' => '' 
+					'success_msg' => ''
 				);
 			}
 		}
 		// Forms have been imported.
-		
+
 		// Insert notifications:
 		foreach ( $notifications_array as $form_id => $notification_array ) {
 			foreach ( $notification_array as $x => $n ) {
@@ -372,13 +372,13 @@ class Boldgrid_Ninja_Forms {
 		}
 		// All notifications have been imported.
 	}
-	
+
 	/**
 	 * Get form markup
 	 *
 	 * @static
 	 *
-	 * @param int $form_id        	
+	 * @param int $form_id
 	 *
 	 * @return string
 	 */
@@ -387,7 +387,7 @@ class Boldgrid_Ninja_Forms {
 			return ninja_forms_return_echo( 'ninja_forms_display_form', $form_id );
 		}
 	}
-	
+
 	/**
 	 * Add prebuilt forms
 	 *
@@ -395,10 +395,10 @@ class Boldgrid_Ninja_Forms {
 	 */
 	public function modify_starter_forms() {
 		self::add_prebuilt_forms();
-		
+
 		return '';
 	}
-	
+
 	/**
 	 * Get forms
 	 *
@@ -409,30 +409,30 @@ class Boldgrid_Ninja_Forms {
 	public static function get_forms() {
 		// Todo:
 		// ninja_forms_get_all_forms());
-		
+
 		// Connect to the WordPress database:
 		global $wpdb;
-		
+
 		// Query the database:
-		$results = $wpdb->get_results( 
+		$results = $wpdb->get_results(
 			"SELECT distinct form_id FROM {$wpdb->prefix}ninja_forms_fields", OBJECT );
-		
+
 		// Initialize $form_ids array:
 		$form_ids = array ();
-		
+
 		// Populate the $form_ids array:
 		foreach ( $results as $result ) {
 			if ( ! empty( $result->form_id ) ) {
 				$form_ids[] = array (
-					'id' => $result->form_id 
+					'id' => $result->form_id
 				);
 			}
 		}
-		
+
 		// Return the resulting array:
 		return $form_ids;
 	}
-	
+
 	/**
 	 * Create Tabs based on configurations
 	 *
@@ -443,25 +443,25 @@ class Boldgrid_Ninja_Forms {
 			require_once $this->path_configs['plugin_dir'] .
 				 '/boldgrid/includes/class-boldgrid-ninja-forms-media-tab.php';
 		}
-		
+
 		require_once $this->path_configs['plugin_dir'] .
 			 '/boldgrid/includes/class-boldgrid-ninja-forms-media-tab-form.php';
-		
+
 		$boldgrid_configs = $this->get_tab_configs();
-		
+
 		$configs = $boldgrid_configs['tabs'];
-		
+
 		/**
 		 * Create each tab specified from the configuration.
 		 */
 		foreach ( $configs as $tab ) {
-			$media_tab = new Boldgrid_Ninja_Forms_Media_Tab_Form( $tab, $this->get_path_configs(), 
+			$media_tab = new Boldgrid_Ninja_Forms_Media_Tab_Form( $tab, $this->get_path_configs(),
 				'/boldgrid' );
-			
+
 			$media_tab->create();
 		}
 	}
-	
+
 	/**
 	 * Get Templates for all forms and print them to the page
 	 *
@@ -469,19 +469,19 @@ class Boldgrid_Ninja_Forms {
 	 */
 	public function print_media_templates() {
 		$form_markup = array ();
-		
+
 		$forms = self::get_forms();
-		
+
 		foreach ( $forms as $form ) {
 			$form_markup[$form['id']] = self::get_form_markup( $form['id'] );
 		}
-		
+
 		include BOLDGRID_NINJA_FORMS_PATH . '/boldgrid/includes/partial-page/form-not-found-tmpl.php';
-		
+
 		foreach ( $form_markup as $form_id => $markup ) {
 			$markup = str_replace( '<script', '<# print("<sc" + "ript"); #>', $markup );
 			$markup = str_replace( '</script>', '<# print("</scr" + "ipt>"); #>', $markup );
-			
+
 			?>
 <script type="text/html"
 	id="tmpl-editor-boldgrid-form-<?php echo $form_id; ?>">
@@ -490,7 +490,7 @@ class Boldgrid_Ninja_Forms {
 <?php
 		}
 	}
-	
+
 	/**
 	 * Hide the Ninja Forms Preview Page from the list of pages.
 	 *
@@ -502,26 +502,26 @@ class Boldgrid_Ninja_Forms {
 	 */
 	public function remove_ninja_preview_page( $query ) {
 		global $pagenow;
-		
+
 		// Check that we are on admin page edit.php editing pages
 		if ( ( 'edit.php' == $pagenow && isset( $_GET['post_type'] ) && 'page' == $_GET['post_type'] ) ) {
-			
+
 			// Do not display post with ninja_forms_preview_page $page->ID
 			$page = get_page_by_title( 'ninja_forms_preview_page' );
-			
+
 			if ( is_object( $page ) ) {
 				// Other plugins may set 'post__not_in' as well, and override our setting below.
 				// We'll use array_merge and $query->get so to play nice with other plugins.
-				$query->set( 'post__not_in', 
+				$query->set( 'post__not_in',
 					array_merge( array (
-						$page->ID 
+						$page->ID
 					), $query->get( 'post__not_in' ) ) );
 			}
-			
+
 			return $query;
 		}
 	}
-	
+
 	/**
 	 * Remove Ninja Form preview page from page counts.
 	 *
@@ -532,7 +532,7 @@ class Boldgrid_Ninja_Forms {
 	 * We still need to remove it from the page counts (mentioned immediately above).
 	 *
 	 * @since 1.0.4
-	 *       
+	 *
 	 * @param object $counts
 	 *        	An object of post_types and the count of each. Example $counts:
 	 *        	http://pastebin.com/WrurLkXW
@@ -545,39 +545,39 @@ class Boldgrid_Ninja_Forms {
 		if ( 'page' != $type ) {
 			return $counts;
 		}
-		
+
 		// Get the Ninja Forms Preview page. We need it to find the post's status.
 		$preview_page = get_page_by_title( 'ninja_forms_preview_page' );
-		
+
 		// If no page is found, abort.
 		if ( is_null( $preview_page ) ) {
 			return $counts;
 		}
-		
+
 		$post_status = $preview_page->post_status;
-		
+
 		// Generally the preview page is a 'draft', but we'll make no assumptions. No matter what
 		// the status is, remove it from that status' count.
 		if ( isset( $counts->$post_status ) ) {
 			$counts->$post_status --;
-			
+
 			global $pagenow;
-			
+
 			$current_user_is_author = ( $preview_page->post_author == get_current_user_id() );
 			$preview_page_is_trashed = ( 'trash' == $preview_page->post_status );
-			
+
 			// One count type not listed in $counts is 'Mine', the number of pages authored by the
 			// current user. To update this number, we'll include the below javascript file. We'll
 			// only include this file if the preview page is not trashed, because trashed pages
 			// don't show in the 'Mine' count. Including this js file when we shouldn't will cause
 			// the 'Mine' count to be innacurate.
 			if ( 'edit.php' == $pagenow && $current_user_is_author && ! $preview_page_is_trashed ) {
-				wp_enqueue_script( 'bgnf-all-pages', 
-					plugins_url( '/boldgrid/assets/js/all-pages.js', 
+				wp_enqueue_script( 'bgnf-all-pages',
+					plugins_url( '/boldgrid/assets/js/all-pages.js',
 						$this->path_configs['plugin_filename'] ), array (), BOLDGRID_NINJA_FORM_VERSION );
 			}
 		}
-		
+
 		return $counts;
 	}
 }
