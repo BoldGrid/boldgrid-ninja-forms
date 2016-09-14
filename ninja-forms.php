6375fececc934be0778ab8a95d5dde937b9219da
Tim <dev@tim.ph>
2016-09-14 12:40:22 -0400

    set version constant from plugin file

--- ninja-forms.php
+++ ninja-forms.php
@@ -44,7 +44,7 @@ if ( false === defined( 'ABSPATH' ) )
 
 // Define Form version.
 if ( false === defined( 'BOLDGRID_NINJA_FORM_VERSION' ) ) {
-	define( 'BOLDGRID_NINJA_FORM_VERSION', '1.2.2' );
+	define( 'BOLDGRID_NINJA_FORM_VERSION', implode( get_file_data( __FILE__, array( 'Version' ), 'plugin' ) ) );
 }
 
 // Define boldgrid-ninja-forms Path.
