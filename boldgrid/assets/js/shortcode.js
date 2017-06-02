fa51ba78e7003d2dedb5c3034bc1d96a3c201dc8
Rafael Ramos <rramo012@gmail.com>
2017-06-02 13:05:25 -0400

    Formatting changes

--- boldgrid/assets/js/shortcode.js
+++ boldgrid/assets/js/shortcode.js
@@ -2,7 +2,7 @@
 	var postID = $( '#post_ID' ).val() || 0,
 		media, boldgrid_form;
 
-    var boldgrid_edit_form = jQuery.Event('boldgrid_edit_form');
+    var boldgrid_edit_form = jQuery.Event( 'boldgrid_edit_form' );
 
 	media = {
 		state: [],
@@ -16,12 +16,11 @@
 
 			//This value will be read from within an iframe, but should be reset to null
 			//to prevent going into edit mode.
-			//
 			setTimeout( function() {
 				IMHWPB.editform = null;
 			}, 10000 );
 
-			$(window).trigger(boldgrid_edit_form, this);
+			$( window ).trigger( boldgrid_edit_form, this );
 		}
 	};
 
@@ -32,18 +31,18 @@
 
 		initialize: function() {
 			var options = this.shortcode.attrs.named;
-			var desc = options.description == "true" ? '1' : '0';
-			var title = options.title == "true" ? '1' : '0';
+			var desc = options.description == 'true' ? '1' : '0';
+			var title = options.title == 'true' ? '1' : '0';
 
 			var current_selector = 'editor-boldgrid-form-' + options.id;
 			if ( $( '#tmpl-' + current_selector ).length ) {
 				this.template = wp.media.template( current_selector );
 
-				this.render( "<div class='boldgrid-ninja-form' data-id='" +
-					options.id + "' data-description=" + desc +
-					" data-title=" + title + ">" + this.template() + "</div>" );
+				this.render( '<div class=\'boldgrid-ninja-form\' data-id=\'' +
+					options.id + '\' data-description=' + desc +
+					' data-title=' + title + '>' + this.template() + '</div>' );
 
-				setTimeout( function () {
+				setTimeout( function() {
 					if ( tinymce && tinymce.activeEditor ) {
 						$( tinymce.activeEditor.iframeElement ).contents()
 							.find( '.boldgrid-ninja-form[data-id="' + options.id + '"]' )
@@ -56,7 +55,7 @@
 				this.template = wp.media.template( 'editor-boldgrid-not-found' );
 				this.render( this.template() );
 			}
-		},
+		}
 	} );
 
 	views.register( 'ninja_forms', _.extend( {}, boldgrid_form ) );
@@ -64,8 +63,8 @@
 	/**
 	 * Before Bold grid Initializes add the menu items
 	 */
-	jQuery(document).on('BoldGridPreInit', function ( event, wp_mce_draggable ) {
-		wp_mce_draggable.add_menu_item( 'Insert Form', 'column', function () {
+	jQuery( document ).on( 'BoldGridPreInit', function( event, wp_mce_draggable ) {
+		wp_mce_draggable.add_menu_item( 'Insert Form', 'column', function() {
 			//On click of the new form, Open the media modal to the forms tab
 			wp_mce_draggable.insert_from_media_modal_tab( 'iframe:boldgrid_form' );
 		} );
